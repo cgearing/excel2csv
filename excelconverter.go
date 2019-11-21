0f14876853aa74fa4f9d2bab39274f99512e62f0
Chris Gearing <gearichr@turntown.com>
2019-11-21 09:13:54 +0000

    update excelize to v2 and use go mod` `

--- excelconverter.go
+++ excelconverter.go
@@ -9,7 +9,7 @@ import (
 	"path"
 	"path/filepath"
 
-	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/360EntSecGroup-Skylar/excelize/v2"
 )
 
 func writeXLasCSV(filename string, ignoreHidden bool) error {
@@ -29,12 +29,15 @@ func writeXLasCSV(filename string, ignoreHidden bool) error {
 		newFileName := fmt.Sprintf("output/%0s-%1s.csv", fileNameNoExt, sheetName)
 		newFile, err := os.OpenFile(newFileName, os.O_RDWR|os.O_CREATE, 0755)
 		if err != nil {
-			fmt.Println("can't create new file", newFileName)
 			return err
 		}
 		csvWriter := csv.NewWriter(newFile)
 		fmt.Println(index, sheetName)
-		err = csvWriter.WriteAll(file.GetRows(sheetName))
+		rows, err := file.GetRows(sheetName)
+		if err != nil {
+			return err
+		}
+		err = csvWriter.WriteAll(rows)
 		if err != nil {
 			return err
 		}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module excel2csv
 
 go 1.13
 
-require github.com/360EntSecGroup-Skylar/excelize v1.4.1
+require github.com/360EntSecGroup-Skylar/excelize/v2 v2.0.2
